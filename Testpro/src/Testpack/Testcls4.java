b82ced84bbbba13beb3f4eb4daa3b2f66b4d7fb5
Safeena <Safeena@Welcome>
2018-07-09 12:13:19 +0530

    edited class 3 and added 2 new classes

--- Testpro/src/Testpack/Testcls4.java
+++ Testpro/src/Testpack/Testcls4.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Testcls3 {
-
+public class Testcls {
     @Test
 	public void Tc1()
 	{
@@ -18,9 +17,8 @@ public class Testcls3 {
 		driver.get("https://www.amazon.in/");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		WebElement e1 =driver.findElement(By.xpath("//*[@class='nav-a nav-a-2']/span[contains(text(),'Your Orders')]"));
-		//e1.click();
-		//driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		e1.click();
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 		driver.quit();
 	}
-
 }
